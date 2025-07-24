d02dbb07de8433731a867a4e674380d7c8ad65f0
little loach <lyrz2023@gmail.com>
2025-07-24 09:22:02 +0800

    implement coordinator first

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,24 +1,74 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Stage int
+
+const (
+	MapReady    Stage = iota // 0
+	ReduceReady              // 1
+	AllDone                  // 2
+)
 
 type Coordinator struct {
-	// Your definitions here.
+	files      []string
+	nReduce    int
+	stage      Stage
+	taskQueue  chan Task
+	taskActive map[int]chan struct{}
+	taskId     atomic.Int64
+	wg         sync.WaitGroup
+}
+
+// Assign map and reduce tasks to workers
+// This function is called by the worker to get a task.
+func (c *Coordinator) AssignTask(args *AssignTaskArgs, reply *AssignTaskReply) error {
+	if c.stage != AllDone {
+		task := <-c.taskQueue
+		reply.Task = task
+		c.taskActive[task.TaskID] = make(chan struct{})
+		go func(taskActive chan struct{}) {
+			timer := time.NewTimer(10 * time.Second)
+			defer timer.Stop()
+
+			// Wait for the worker to complete the task or timeout
+			select {
+			case <-taskActive:
+				return
+			case <-timer.C:
+				c.taskQueue <- task
+			}
+		}(c.taskActive[task.TaskID])
+	} else {
+		c.Done()
+	}
+	return nil
 
 }
 
-// Your code here -- RPC handlers for the worker to call.
-
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
+// Mark a task as complete.
+// The function signals that the task is complete and decrements the wait group counter.
+// If the task is not found or already completed, it returns an error.
+func (c *Coordinator) TaskComplete(args *TaskCompleteArgs, reply *TaskCompleteReply) error {
+	taskID := args.TaskID
+	taskActive := c.taskActive[taskID]
+	if taskActive != nil {
+		taskActive <- struct{}{} // Signal that the task is complete
+		c.wg.Done()              // Decrement the wait group counter
+		return nil
+	} else {
+		return errors.New("task not found or already completed")
+	}
 }
 
 // start a thread that listens for RPCs from worker.go
@@ -38,10 +88,7 @@ func (c *Coordinator) server() {
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
+	ret := (c.stage == AllDone)
 	return ret
 }
 
@@ -49,9 +96,54 @@ func (c *Coordinator) Done() bool {
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		files:      files,
+		nReduce:    nReduce,
+		stage:      MapReady,
+		taskId:     atomic.Int64{},
+		taskQueue:  make(chan Task, nReduce),
+		taskActive: make(map[int]chan struct{}),
+		wg:         sync.WaitGroup{},
+	}
+	c.wg.Add(len(files))
 
-	// Your code here.
+	// Initialise map tasks for each file
+	go func(files []string, nReduce int) {
+		for _, file := range files {
+			mapTask := Task{
+				TaskType: MapTask,
+				TaskID:   int(c.taskId.Load()),
+				Filename: file,
+				ReduceID: nReduce,
+			}
+			c.taskId.Add(1)
+			c.taskQueue <- mapTask
+		}
+		c.wg.Wait()
+		c.stage = ReduceReady
+		c.wg.Add(nReduce)
+
+		// Initialise reduce tasks
+		for i := range nReduce {
+			reduceTask := Task{
+				TaskType: ReduceTask,
+				TaskID:   int(c.taskId.Load()),
+				Filename: "",
+				ReduceID: i,
+			}
+			c.taskId.Add(1)
+			c.taskQueue <- reduceTask
+		}
+
+		c.wg.Wait()
+		c.stage = AllDone
+
+		// Close the task queue and active channels
+		close(c.taskQueue)
+		for _, taskActive := range c.taskActive {
+			close(taskActive)
+		}
+	}(files, nReduce)
 
 	c.server()
 	return &c
